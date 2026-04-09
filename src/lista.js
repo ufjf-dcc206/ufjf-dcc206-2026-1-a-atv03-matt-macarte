4e77e5fe06cb85d56958860c9b2293c42b982a49
matt-macarte <matt.almeidac@gmail.com>
2026-04-08 22:20:35 -0300

    :test_tube: add teste falho pra lista

--- src/lista.js
+++ src/lista.js
@@ -1,5 +1,14 @@
-const lista = ["um", "dois", "tres"];
+let lista = [];
+
+export function iniciaLista(){
+    lista = ["um", "dois", "tres"];
+}
 
 export function getLista(){
     return structuredClone(lista);
+}
+
+export function limpaLista(){
+    lista.splice(0,3);
+    return lista;
 }
--- src/lista.test.js
+++ src/lista.test.js
@@ -1,11 +1,17 @@
 import { expect, it } from "vitest";
-import { getLista } from "./lista.js";
+import { iniciaLista, getLista, limpaLista } from "./lista.js";
 
 it("deve ter elementos iniciais", () => {
+    iniciaLista();
     const lista1 = getLista();
     expect(lista1.length).toBe(3);
     lista1.push("quatro");
     const lista2 = getLista();
     expect(lista2.length).toBe(3);
 });
+
+it("deve limpar a lista", () => {
+    const limpa = limpaLista();
+    expect(limpa[0]).toBe(0);
+})
 
